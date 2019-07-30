f0e69c7492705aec008c0e48b97bc1fb1f193da5
2paradox <dltjdcjf93@pusan.ac.kr>
2019-07-30 23:32:59 +0900

    blog

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\lee seong cheol\desktop\likelion\해커톤\blog-portfolio\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
